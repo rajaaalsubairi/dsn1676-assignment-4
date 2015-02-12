5894e8cd977d301fcc3e43d877bc9a0fe2c11e86
rajaaalsubairi <rajaaal-subairi@Rajaa.local>
2015-02-12 16:33:38 -0500

    styled and added javascript

---
+++ js/main.js
@@ -0,0 +1,48 @@
+var berries = {
+    fullName: 'Vaccinium corymbosum',
+    growsOnShrub: true,
+    energy: 240,
+    carbohydrates: 14.49,
+    protein: 0.74
+};
+
+var grapes = {
+    fullName: 'Vitis vinifera',
+    growsOnShrub: false,
+    energy: 288,
+    carbohydrates: 18.1,
+    protein: 0.72
+};
+
+var currant = {
+    fullName: 'Ribes rubrum',
+    growsOnShrub: true,
+    energy: 234,
+    carbohydrates: 13.8,
+    protein: 1.4
+};
+
+
+var writeFruit = function (fruit) {
+    document.write('<h1>' + fruit.fullName + '</h1>');
+    
+    document.write('<dl>');
+    
+    document.write('<dt>Energy</dt>');
+    document.write('<dd>' + fruit.energy + '</dd>');
+    
+    document.write('<dt>Carbohydrate</dt>');
+    document.write('<dd>' + fruit.carbohydrates + '</dd>');
+    
+    document.write('<dt>Grows On Shrub</dt>');
+    document.write('<dd>' + fruit.growsOnShrub + '</dd>');
+    
+    document.write('<dt>Protein</dt>');
+    document.write('<dd>' + fruit.protein + '</dd>');
+    
+    document.write('</dl>');
+};
+
+var allFruit = [berries, grapes, currant];
+
+allFruit.forEach(writeFruit);
